acb2e5374ebdfb6e7aa267baba00e75607b95058
Roberto <r.attanasio90@gmail.com>
2021-11-19 09:38:36 +0100

    Struttura e grafica di base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// ROUTING MODULE
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// HEADER COMPONENT
+import { HeaderComponent } from './header/header.component';
+import { GameComponent } from './game/game.component';
+import { LetterBoxComponent } from './game/letter-box/letter-box.component';
+import { InputBoxComponent } from './game/input-box/input-box.component';
+import { ButtonBoxComponent } from './game/button-box/button-box.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, GameComponent, LetterBoxComponent, InputBoxComponent, ButtonBoxComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/game/letter-box/letter-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-letter-box',
+  templateUrl: './letter-box.component.html',
+  styleUrls: ['./letter-box.component.css']
+})
+export class LetterBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
